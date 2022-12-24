69ff1ac1d5274e9ea39ce15def9a75d79d25c043
Alejo Ufano <ufanoalejo@gmail.com>
2022-12-24 14:48:13 -0300

    Fix(Feature) Feature component added

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -16,8 +16,7 @@ const Header = () => {
         <div className="gpt3__header-content__people">
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
-        </div>
-        
+        </div>        
       </div>
       <div className="gpt3__header-img">
         <img src={ai} alt="ai" />
